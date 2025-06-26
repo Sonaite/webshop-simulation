1adcb284779303fc8b37328d807667430afa6be4
Nathy <sonaite.santos093@academico.ifs.edu.br>
2025-06-25 22:32:12 -0300

    Criação de menu

--- back/src/main/java/webshop/simulation/Repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package webshop.simulation.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import webshop.simulation.Models.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
--- back/src/main/java/webshop/simulation/Repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package webshop.simulation.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import webshop.simulation.Models.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByEmail(String email);
-}
